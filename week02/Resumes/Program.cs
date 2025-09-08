13c6364bce206d35bbb81467d049204725541d1b
daniely998 <danzone998@gmail.com>
2025-09-08 12:19:28 +0800

    week02 resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Old Eagle";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2070;
+        job1._endYear = 2074;
+
+        Job job2 = new Job();
+        job2._company = "Wenlen";
+        job2._jobTitle = "Software Engineer";
+        job2._startYear = 2074;
+        job2._endYear = 2077;
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
