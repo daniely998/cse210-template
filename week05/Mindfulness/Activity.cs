fedbf254b4de62c14635ad0ae46750fd4d4866e4
daniely998 <danzone998@gmail.com>
2025-09-30 18:54:25 +0800

    stub

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,11 @@
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity() { }
+    public void DisplayStartingMessage() { }
+    public void DisplayEndingMessage() { }
+    public void ShowCountDown() {}
+}
---
+++ week05/Mindfulness/BreathingActivity.cs
@@ -0,0 +1,5 @@
+public class BreathingActivity : Activity
+{
+    public BreathingActivity() { }
+    public void Run() {}
+}
