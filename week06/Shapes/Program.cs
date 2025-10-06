b3e180e05c74fb1dbba001e8671928ecfba5a7cc
daniely998 <danzone998@gmail.com>
2025-10-06 14:49:30 +0800

    LA

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new();
+        Square square = new("Yellow", 4);
+        Rectangle rectangle = new("Blue", 3, 4);
+        Circle circle = new("Green", 5);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        foreach (Shape shape in shapes)
+        {
+            System.Console.WriteLine(shape.GetColor());
+            System.Console.WriteLine($"{shape.GetArea():F2}");
+        }
     }
 }
